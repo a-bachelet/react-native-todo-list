38fe88c3c8eb4cab51dc6fe68a3ddba2fe27a884
Alexis Bachelet <app.bachelet@gmail.com>
2021-04-01 12:43:32 +0200

    feat(new task): add new task section

--- todoList/App.tsx
+++ todoList/App.tsx
@@ -1,19 +1,53 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
+  // Data
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  // States
+  const [task, setTask] = useState<string>("");
+
+  // Logic
+  const addTask = () => {
+    setTasks([...tasks, task.toString()]);
+    setTask("");
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.tasksSection}>
         <Text style={styles.title}>Todo today</Text>
         <View style={styles.tasksList}>
-          <Task text="😂 First Task"></Task>
-          <Task text="🚀 Second Task"></Task>
-          <Task text="🐱‍🐉 Third Task"></Task>
+          {tasks.map((t) => {
+            return <Task text={t} key={t.toString()}></Task>;
+          })}
         </View>
       </View>
-    </View>
+      <View style={styles.bottomContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Add a task"
+          onChangeText={(newText) => setTask(newText)}
+          value={task}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={addTask}>
+          <View style={styles.addButtonView}>
+            <Text style={styles.addButtonText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -21,6 +55,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#8C82FF",
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
   tasksSection: {
     paddingTop: 80,
@@ -34,5 +70,45 @@ const styles = StyleSheet.create({
   tasksList: {
     flexDirection: "column",
     alignItems: "stretch",
+  },
+  bottomContainer: {
+    margin: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textInput: {
+    height: 45,
+    backgroundColor: "#FFF",
+    width: 246,
+    borderRadius: 60,
+    textAlign: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 4 },
+    shadowRadius: 20,
+    shadowOpacity: 0.1,
+  },
+  addButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    backgroundColor: "#FFF",
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 4 },
+    shadowRadius: 20,
+    shadowOpacity: 0.1,
+    width: 60,
+    height: 60,
+  },
+  addButtonView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addButtonText: {
+    color: "#55BCF6",
+    fontSize: 45,
+    lineHeight: 45,
   },
 });
